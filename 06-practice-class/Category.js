0039a7bdf3ff187d34a806e364333ddc8adf75ba
Tomohiro Meo <h2.adachi.tomohiro@gmail.com>
2021-03-08 12:08:20 -0800

    Add files

---
+++ 06-practice-class/Category.js
@@ -0,0 +1,10 @@
+// change this Component to a class!
+function Category(props){
+	// get data from props and use map to build an array of <li>
+	// ... code goes here
+	return(
+		<div>
+			{/* Your Code Here */}
+		</div>
+	)
+}
